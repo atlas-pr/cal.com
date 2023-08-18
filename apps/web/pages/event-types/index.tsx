98fa0a5a8d5e9c6916cbcd55e285933372da8880
PR <piyush@getatlas.io>
2023-08-18 22:04:29 +0530

    match user id to user.email

--- apps/web/pages/event-types/index.tsx
+++ apps/web/pages/event-types/index.tsx
@@ -943,7 +943,7 @@ const EventTypesPage = () => {
     const { atlasId, isVisitor, email, userId, appId } = getAtlasIdentity();
     console.log(atlasId, isVisitor, email, userId, appId);
     console.log(user);
-    if (user && atlasId && user.email && email !== user.userId) {
+    if (user && atlasId && user.email && user.email !== userId) {
       window.Atlas.identify({
         userId: user.email,
         name: user.name,
